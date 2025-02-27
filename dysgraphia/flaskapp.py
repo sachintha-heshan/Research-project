f15f33f219ee1fa170734d3ae58fbb83dfd823ce
IT21183768 <it21183768@my.sliit.lk>
2025-02-27 08:15:09 +0530

    Load the model for py file

--- dysgraphia/flaskapp.py
+++ dysgraphia/flaskapp.py
@@ -7,3 +7,9 @@ from collections import Counter
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+# Load the trained model
+model = load_model("handwriting_dysgraphia_model.h5")
+
+# Define class labels
+class_labels = ['Low Risk for Dysgraphia', 'High Risk for Dysgraphia']
