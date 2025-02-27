e50c1b075e8bd9873a4ed61439f497eeb9e73ea3
IT21183768 <it21183768@my.sliit.lk>
2025-02-27 08:19:41 +0530

    add threshold

--- dysgraphia/flaskapp.py
+++ dysgraphia/flaskapp.py
@@ -24,3 +24,11 @@ def segment_words(image_path):
         ar = w / h
         new_h = int(new_w / ar)
         img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
+        
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    _, thresh = cv2.threshold(img_gray, 80, 255, cv2.THRESH_BINARY_INV)
+
+    kernel_line = np.ones((3, 85), np.uint8)
+    dilated_line = cv2.dilate(thresh, kernel_line, iterations=1)
+    contours_line, _ = cv2.findContours(dilated_line, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    sorted_lines = sorted(contours_line, key=lambda ctr: cv2.boundingRect(ctr)[1])
