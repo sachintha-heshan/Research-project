d509eb78f8c0fae848a0ebb2b87e6b3bbca52a79
IT21183768 <it21183768@my.sliit.lk>
2025-02-27 08:17:54 +0530

    create word segment fun

--- dysgraphia/flaskapp.py
+++ dysgraphia/flaskapp.py
@@ -13,3 +13,14 @@ model = load_model("handwriting_dysgraphia_model.h5")
 
 # Define class labels
 class_labels = ['Low Risk for Dysgraphia', 'High Risk for Dysgraphia']
+
+def segment_words(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    h, w, c = img.shape
+    if w > 1000:
+        new_w = 1000
+        ar = w / h
+        new_h = int(new_w / ar)
+        img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
